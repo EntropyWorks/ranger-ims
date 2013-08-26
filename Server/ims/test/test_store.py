3ece2362eb32fa56898f021a7d79389e1e27c970
Wilfredo Sánchez Vega <wsanchez@wsanchez.net>
2013-08-26 10:08:01 -0700

    Start tests

---
+++ Server/ims/test/test_store.py
@@ -0,0 +1,45 @@
+##
+# See the file COPYRIGHT for copyright information.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Tests for L{ims.store}.
+"""
+
+import twisted.trial.unittest
+from twisted.python.filepath import FilePath
+
+from ims.store import Storage
+
+
+class StoreTests(twisted.trial.unittest.TestCase):
+    """
+    Tests for L{ims.store.Storage}
+    """
+
+    def storage(self):
+        return Storage(FilePath(self.mktemp()))
+
+
+    def test_provision(self):
+        store = self.storage()
+        self.assertFalse(store.path.isdir())
+        store.provision()
+        self.assertTrue(store.path.isdir())
+
+
+    def test_list(self):
+        store = self.storage()
+        self.assertEquals(set(store.list_incidents()), set())
